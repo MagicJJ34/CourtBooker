949f9a248547c01b1df128f13248ebd6f50bdf68
MagicJJ34 <milosz.makiel3@gmail.com>
2026-09-08 19:03:04 +0200

    feat: configure AppDbContext and entity relationships

--- CourtBooker/Data/AppDbContext.cs
+++ CourtBooker/Data/AppDbContext.cs
@@ -1,6 +1,30 @@
-﻿namespace CourtBooker.Data
+﻿using CourtBooker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourtBooker.Data;
+
+public class AppDbContext : DbContext
 {
-    public class AppDbContext
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Court> Courts { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Reservation>()
+            .HasOne(r => r.User)
+            .WithMany(u => u.Reservations)
+            .HasForeignKey(r => r.UserId);
+
+        modelBuilder.Entity<Reservation>()
+            .HasOne(r => r.Court)
+            .WithMany(c => c.Reservations)
+            .HasForeignKey(r => r.CourtId);
     }
 }
