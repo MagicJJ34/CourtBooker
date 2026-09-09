18a3801a326d3fbaa0cabe3f6933a26b368ac3c9
MagicJJ34 <milosz.makiel3@gmail.com>
2026-09-09 22:40:50 +0200

    feat: add court management (Courts Controller, Index/Create views, TagHelper configuration)

---
+++ CourtBooker/Controllers/CourtsController.cs
@@ -0,0 +1,43 @@
+﻿using CourtBooker.Data;
+using CourtBooker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourtBooker.Controllers;
+
+public class CourtsController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public CourtsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var courts = await _context.Courts.ToListAsync();
+        return View(courts);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Name,Surface,PricePerHour")] Court court)
+    {
+        ModelState.Remove("Reservations");
+
+        if (ModelState.IsValid)
+        {
+            _context.Add(court);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(court);
+    }
+}
---
+++ CourtBooker/Views/Courts/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CourtBooker.Views.Courts
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
